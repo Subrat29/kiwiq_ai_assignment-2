37b8350f10e0ae0e0e52f4145a344ad21b325e8f
Subrat29 <ankubanku29062003@gmail.com>
2024-12-29 19:36:50 +0530

    feat: integrate Redux for state management; add flowSlice and create store; implement useNodeId hook and workflow nodes

--- src/components/WorkflowBuilder.jsx
+++ src/components/WorkflowBuilder.jsx
@@ -1,45 +1,15 @@
-import React, { useState } from 'react';
-import ReactFlow, { addEdge, Background, Controls } from 'react-flow-renderer';
+import { PipelineToolbar } from './WorkflowBuilder/toolbar';
+import { PipelineUI } from './WorkflowBuilder/ui';
+import { SubmitButton } from './WorkflowBuilder/submit';
 
-const WorkflowBuilder = () => {
-  const [nodes, setNodes] = useState([
-    { id: '1', type: 'input', data: { label: 'Start' }, position: { x: 250, y: 5 } },
-  ]);
-  const [edges, setEdges] = useState([]);
-
-  const onNodesChange = (changes) =>
-    setNodes((nds) =>
-      nds.map((node) => {
-        const change = changes.find((c) => c.id === node.id);
-        return change ? { ...node, ...change } : node;
-      })
-    );
-
-  const onEdgesChange = (changes) =>
-    setEdges((eds) =>
-      eds.map((edge) => {
-        const change = changes.find((c) => c.id === edge.id);
-        return change ? { ...edge, ...change } : edge;
-      })
-    );
-
-  const onConnect = (params) => setEdges((eds) => addEdge(params, eds));
-
+function WorkflowBuilder() {
   return (
-    <div className="h-screen bg-gray-100">
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        fitView
-      >
-        <Background color="#aaa" gap={16} />
-        <Controls />
-      </ReactFlow>
+    <div>
+      <PipelineToolbar />
+      <PipelineUI />
+      <SubmitButton />
     </div>
   );
-};
+}
 
 export default WorkflowBuilder;
